3555638469c787dcadaab4a16364fc79a41d6024
Leinad <daniel.jauregui@outlook.com>
2016-06-16 01:49:05 -0400

    Fill all required fields but needs improve the phone number, currently it is filling incorrect

--- Daniel/Task0615/features/step_definitions/form_steps.rb
+++ Daniel/Task0615/features/step_definitions/form_steps.rb
@@ -5,17 +5,41 @@ end
 Given(/^I fill following data:$/) do |input|
 	@form_data = input.raw
 	@form_data.each do |item|
-		#page.find(:fillable_field, '02frstname').set(item[0])
 		page.find_field('02frstname',:type => "text").set(item[0])
+		page.find_field('04lastname',:type => "text").set(item[1])
+		page.find_field('20homephon_ac',:type => "text").set(item[2])
+		page.find_field('20homephon_pr',:type => "text").set(item[2])
+		page.find_field('20homephon_sf',:type => "text").set(item[2])
+		page.find_field('10address1',:type => "text").set(item[3])
+		page.find_field('13adr_city',:type => "text").set(item[4])
+		page.find(:xpath, '/html/body/div[3]/div/div/div/form/table[1]/tbody/tr[4]/td[2]/select').select(item[5])
+		page.find_field('16addr_zip',:type => "text").set(item[6])
 	end
+	expect(page.find_field('02frstname',:type => "text").value()).not_to be_empty
+	expect(page.find_field('04lastname',:type => "text").value()).not_to be_empty
+	expect(page.find_field('20homephon_ac',:type => "text").value()).not_to be_empty
+	expect(page.find_field('20homephon_pr',:type => "text").value()).not_to be_empty
+	expect(page.find_field('20homephon_sf',:type => "text").value()).not_to be_empty
+	expect(page.find_field('10address1',:type => "text").value()).not_to be_empty
+	expect(page.find_field('13adr_city',:type => "text").value()).not_to be_empty
+	expect(page.find(:xpath, '/html/body/div[3]/div/div/div/form/table[1]/tbody/tr[4]/td[2]/select').value()).not_to be_empty
+	expect(page.find_field('16addr_zip',:type => "text").value()).not_to be_empty
 end
 
-When(/^I will click in "([^"]*)" button$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I will click in "([^"]*)" button$/) do |button|
+	page.find_button('Reset').click()
 end
 
 Then(/^The all field will be in initial state$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	expect(page.find_field('02frstname',:type => "text").value()).to be_empty
+	expect(page.find_field('04lastname',:type => "text").value()).to be_empty
+	expect(page.find_field('20homephon_ac',:type => "text").value()).to be_empty
+	expect(page.find_field('20homephon_pr',:type => "text").value()).to be_empty
+	expect(page.find_field('20homephon_sf',:type => "text").value()).to be_empty
+	expect(page.find_field('10address1',:type => "text").value()).to be_empty
+	expect(page.find_field('13adr_city',:type => "text").value()).to be_empty
+	expect(page.find(:xpath, '/html/body/div[3]/div/div/div/form/table[1]/tbody/tr[4]/td[2]/select').value()).to be_empty
+	expect(page.find_field('16addr_zip',:type => "text").value()).to be_empty
 end
 
 
